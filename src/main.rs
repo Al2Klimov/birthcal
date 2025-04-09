c6165d7b048f5daf127abfcd0af8e2430f1d7fb1
Alexander A. Klimov <a@kli.mov>
2025-04-09 07:08:42 +0200

    Order by month/day/year/name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod cli;
 
 use cgi::{empty_response, handle, text_response, Request, Response};
+use chrono::{Datelike, NaiveDate};
 use ical::parser::vcard::component::VcardContact;
 use ical::parser::Component;
+use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::Write;
 use std::io::BufReader;
 
@@ -25,7 +27,8 @@ fn handler(_: Request) -> Response {
             empty_response(502)
         }
         Ok(mut resp) => {
-            let mut body = String::new();
+            let mut names_by_mdy =
+                BTreeMap::<u32, BTreeMap<u32, BTreeMap<i32, BTreeSet<String>>>>::new();
 
             for i in ical::VcardParser::new(BufReader::new(resp.body_mut().as_reader())) {
                 match i {
@@ -37,15 +40,49 @@ fn handler(_: Request) -> Response {
                         None => {}
                         Some(birthday) => match contact_prop(&vcard, "FN") {
                             None => {}
-                            Some(name) => match writeln!(body, "{} {}", birthday, name) {
+                            Some(name) => {
+                                let df = "%Y%m%d";
+
+                                match NaiveDate::parse_from_str(
+                                    birthday.replace("-", "").as_str(),
+                                    df,
+                                ) {
+                                    Err(err) => {
+                                        eprintln!("{} is not like {}: {}", birthday, df, err);
+                                        return empty_response(502);
+                                    }
+                                    Ok(date) => {
+                                        names_by_mdy
+                                            .entry(date.month())
+                                            .or_default()
+                                            .entry(date.day())
+                                            .or_default()
+                                            .entry(date.year())
+                                            .or_default()
+                                            .replace(name.clone());
+                                    }
+                                }
+                            }
+                        },
+                    },
+                }
+            }
+
+            let mut body = String::new();
+
+            for (month, names_by_dy) in names_by_mdy {
+                for (day, names_by_year) in names_by_dy {
+                    for (year, names) in names_by_year {
+                        for name in names {
+                            match writeln!(body, "{}-{}-{} {}", year, month, day, name) {
                                 Err(err) => {
                                     eprintln!("{}", err);
                                     return empty_response(500);
                                 }
                                 Ok(_) => {}
-                            },
-                        },
-                    },
+                            }
+                        }
+                    }
                 }
             }
 
